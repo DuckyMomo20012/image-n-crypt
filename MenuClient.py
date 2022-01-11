cdaeaec2ab30169f928b633a5db3ddb62dd13884
nameTun2001 <87757481+nameTun2001@users.noreply.github.com>
2022-01-11 15:54:43 +0700

    Add files via upload

---
+++ MenuClient.py
@@ -0,0 +1,81 @@
+import sys
+def mainMenu():
+    print("___MENU___")
+    print("1. Login")
+    print("2. Register")
+    print("3. Quit")
+    option = int(input("Enter your option: "))
+    while option != 0:
+        try : 
+            if option == 1:
+                optionLogin()
+            elif option == 2:
+                #optionRegister()
+                print("!!!")
+            elif option == 3:
+                sys.exit()
+            else:
+                print("Invalid choise. Enter 1 - 3")
+                mainMenu()
+        except ValueError:
+            print("Invalid choise. Enter 1 - 3")
+
+def optionLogin():
+    print("____LOGIN___")
+    print("1. List image")
+    print("2. Upload image")
+    print("3. Dowload all images")
+    print("4. Back")
+    option = int(input("Enter your option: "))
+    while option != 0:
+        try : 
+            if option == 1:
+                optionInOptionLogin()
+            elif option == 2:
+                print()
+            elif option == 3:
+                print()
+            elif option == 4:
+                mainMenu()
+            else:
+                print("Invalid choise. Enter 1 - 4")
+                mainMenu()
+        except ValueError:
+            print("Invalid choise. Enter 1 - 4")    
+def back():
+    enter = int(input("Can you back(0.No - 1.Yes)? " ))
+    while enter <2:
+        if enter == 0:
+           sys.exit()
+        elif enter == 1:
+            optionLogin()
+
+def optionInOptionLogin():
+    print("____IMAGE PROCESING___")
+    print("1. Dowload image")
+    print("2. Save image")
+    print("3. Delete images")
+    print("4. Back")
+    option = int(input("Enter your option: "))
+    while option != 0:
+        try : 
+            if option == 1:
+                print()
+                #back()
+            elif option == 2:
+                back()
+            elif option == 3:
+                back()
+            elif option == 4:
+                optionLogin()
+            else:
+                print("Invalid choise. Enter 1 - 4")
+                optionLogin()
+        except ValueError:
+            print("Invalid choise. Enter 1 - 4")    
+
+
+# def optionRegister():
+#     register()
+if __name__== "__main__":
+    mainMenu()
