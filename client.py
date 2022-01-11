6510b2bf0d50c707ac1c3bbf871c92b595069d74
nameTun2001 <87757481+nameTun2001@users.noreply.github.com>
2022-01-11 09:27:15 +0700

    Add files via upload

--- client.py
+++ client.py
@@ -1,31 +1,76 @@
 import requests
 from bs4 import BeautifulSoup
 import json
-
-from requests.sessions import session
+from requests.sessions import session 
+import pickle
 
 url = "http://localhost:5000/api/login"
-# url = "https://zingmp3.vn/"
-data = {"username": "codernothacker", "password": "nopass"}
-res = requests.post(url, json=data)
 
-if res.status_code == 200:
-    print("Success!")
-elif res.status_code == 404:
-    print("Not Found.")
+# save cookie 
+def save_cookies(requests_cookiejar, filename):
+    with open(filename, 'wb') as f:
+        pickle.dump(requests_cookiejar, f)
+# load cookie 
+def load_cookies(filename):
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
 
-print(res.json)
+def client( username, password,filename):
+
+    #save cookies
+    r = requests.get(url)
+    save_cookies(r.cookies, filename)
+
+    #load cookies and do a request
+    client_= requests.get(url, cookies=load_cookies(filename))
+
+    print(client_.status_code)
+
+    if client_.status_code == 200:
+        print ('Success!')
+    elif client_.status_code == 404:
+        print('Not Found.')
+
+    payload = {
+            'username':username, 
+            'password': password
+            }
+    with session() as s:
+        r = requests.post(url, data=payload)
+        print (r)
+        print (r.content)
+
+    r = requests.session()
+
+    data = {'username':'admin', 'password': 'admin'}
+    r = requests.post(url, data = payload)
+    cookie = client_.headers["Set-Cookie"]
+    print( client_.text)  
 
 
-def login(username, password):
-    s = requests.Session()
-    payload = {"username": username, "password": password}
-    res = s.post("http://localhost:5000/api/login", json=payload)
-    s.headers.update({"login": json.loads(res.content)["token"]})
-    print(res.content)
-    return s
+if __name__== "__main__":
+    ans=True
+    while ans:
+        print("  1. Login ")
+        print("  2.Register")
+    
+        ans=input("What would you like to do?" )
+        if ans=="1":
+            ans=True
+            while ans:
+                login() # gọi hàm login 
+                print("  1. List image ")
+                print("  2. Upload image")
+    
+        elif ans=="2":
+            print("\n ...") 
+
+    client()
 
 
-session = login("codernothacker", "nopass")
-r = session.patch("http://localhost:5000/api/login")
-print(r.content)
+
+    
+
+
+
+    
