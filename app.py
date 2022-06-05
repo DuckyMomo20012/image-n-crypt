63c0689b65793154117dbb74158a945fad3e9866
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2022-06-05 18:15:29 +0700

    chore(deps): remove unused deps
    chore: remove unused code

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_mongoengine import MongoEngine
 
-# from flask_login import LoginManager
 from flask_jwt_extended import JWTManager
 from flask_wtf.csrf import CSRFProtect
 from datetime import timedelta
@@ -15,22 +14,22 @@ app = Flask(__name__)
 app.config.from_object("config.Config")
 app.config.from_object("config.DBConfig")
 app.config.from_object("config.JWTConfig")
-# Kinda special, I currently can't set it in class :/
+# Kinda special, I currently can't set it in class 😐️
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = ACCESS_EXPIRES
 
 # UploadSet name is images so config -> UPLOADED_IMAGES_DEST
 app.config["UPLOADED_IMAGES_DEST"] = "src/assets"
 
+# 'IMAGES' for supporting multiple image format
 # images = UploadSet("images", IMAGES)
+# Currently, we only support png
 images = UploadSet("images", ("png"))
 configure_uploads(app, images)
 
-# Auth using cookies
-# login_manager = LoginManager()
-# login_manager.init_app(app)
+# Auth using JWT
 jwt = JWTManager(app)
 
-# CSRF protection: Can't make it works, I gave up
+# CSRF protection
 csrf = CSRFProtect(app)
 
 # DB
