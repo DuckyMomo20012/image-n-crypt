e6ffbcef41fb4d07e0cc21c3c78efc2b55ae0ead
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2022-06-05 21:10:18 +0700

    feat: load secrets from .env file

--- app.py
+++ app.py
@@ -3,22 +3,28 @@ from flask_mongoengine import MongoEngine
 
 from flask_jwt_extended import JWTManager
 from flask_wtf.csrf import CSRFProtect
-from datetime import timedelta
 from flask_uploads import UploadSet, IMAGES, configure_uploads
+from environs import Env
 
-ACCESS_EXPIRES = timedelta(hours=1)
+env = Env()
+# Read .env into os.environ
+env.read_env()  # read .env file, if it exists
 
 app = Flask(__name__)
 
-
-app.config.from_object("config.Config")
-app.config.from_object("config.DBConfig")
-app.config.from_object("config.JWTConfig")
-# Kinda special, I currently can't set it in class 😐️
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = ACCESS_EXPIRES
-
-# UploadSet name is images so config -> UPLOADED_IMAGES_DEST
-app.config["UPLOADED_IMAGES_DEST"] = "src/assets"
+app.config.update(
+    # App configs
+    SECRET_KEY=env.str("SECRET_KEY"),
+    SESSION_COOKIE_SECURE=env.bool("SESSION_COOKIE_SECURE", default=False),
+    UPLOADED_IMAGES_DEST=env.str("UPLOADED_IMAGES_DEST"),
+    WTF_CSRF_ENABLED=env.bool("WTF_CSRF_ENABLED", default=False),
+    FLASK_ENV=env.str("FLASK_ENV", default="production"),
+    # JWT configs
+    JWT_ACCESS_TOKEN_EXPIRES=env.int("JWT_ACCESS_TOKEN_EXPIRES", default=3600),
+    JWT_SECRET_KEY=env.str("JWT_SECRET_KEY"),
+    # MongoDB configs
+    MONGODB_HOST=env.str("MONGODB_HOST"),
+)
 
 # 'IMAGES' for supporting multiple image format
 # images = UploadSet("images", IMAGES)
